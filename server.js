93ef51e1451ed1eeb3e7ddd60e66d06394c1929b
BBT <emmagollan33@gmail.com>
2021-03-07 23:36:25 -0500

    pathways

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.use(express.static("public"));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
+    res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
+    res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
-app.get("/api/notes", (req, res) => {
-    fs.readFile(path.join(__dirname, "/db/db.json"), "utf8", (err, data) => {
+app.get("api/notes", (req, res) => {
+    fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
         if (err) throw err;
         res.json(JSON.parse(data));
     });
@@ -43,16 +43,16 @@ app.post("/api/notes", (req, res) => {
             newDB.push(newNote);
         }
 
-        fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(newDB, null, 2), (err) => {
+        fs.writeFile(path.join(__dirname, "db/db.json"), JSON.stringify(newDB, null, 2), (err) => {
             if (err) throw err;
             res.json(req.body);
         });
     });
 });
 
-app.delete("/api/notes/:id", (req, res) => {
+app.delete("api/notes/:id", (req, res) => {
     const id = parseInt(req.params.id);
-    fs.readFile(path.join(__dirname, "/db/db.json"), "utf8", (err, data) => {
+    fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
         if (err) throw err;
         const db = JSON.parse(data);
         const newDB = [];
@@ -71,7 +71,7 @@ app.delete("/api/notes/:id", (req, res) => {
             }
         }
 
-        fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(newDB, null, 2), (err) => {
+        fs.writeFile(path.join(__dirname, "db/db.json"), JSON.stringify(newDB, null, 2), (err) => {
             if (err) throw err;
             res.json(req.body);
         });
