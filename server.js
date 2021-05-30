7e9bdca8a49ffb2e830551cbdc5994682ab6bce4
Emma Gollan <emmagollan33@gmail.com>
2021-05-30 11:29:37 -0400

    fixing errors

--- server.js
+++ server.js
@@ -1,58 +1,50 @@
-const path = require('path');
-const fs = require('fs');
-const express = require('express');
-const PORT = process.env.PORT || 3001;
+const express = require("express");
+const PORT = process.env.PORT || 3003;
 const app = express();
-const cryptoRandomString = require('crypto-random-string');
+const { v4: uuid } = require("uuid");
+const fs = require("fs");
+const path = require("path");
 
-// parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
 app.use(express.json());
-//file path to the public folder and instruct the server to make these files static resources.
-app.use(express.static('public'));
-
-//returns the index.html file
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
+app.use(express.static("public"));
+// GET returns the notes.html
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+// GET the /api/notes read JSON file and returns saved notes as JSON
+app.get("/api/notes", (req, res) => {
+  res.send(fs.readFileSync("./db/db.json", "utf8"));
+});
+// GET returns the the index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-// returns the notes.html file
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/notes.html'));
+app.post("/api/notes", (req, res) => {
+  let newNote = req.body;
+  newNote.id = uuid();
+  saveNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  saveNotes.push(newNote);
+  fs.writeFileSync(
+    path.join(__dirname, "./db/db.json"),
+    JSON.stringify(saveNotes)
+  );
+  res.json(newNote);
 });
-
-// reads db.json and returns all saved notes as JSON
-app.get('/api/notes', (req, res) => {
-  fs.readFile('./db/db.json', (err, data) => {
-    if (err) throw err;
-    res.json(JSON.parse(data))
-  });
-});
-
-// creates new note with a unique ID
-app.post('/api/notes', (req, res) => {
-  let body = req.body;
-  body.id = cryptoRandomString({ length: 10 });
-  let newNotes = JSON.parse(fs.readFileSync('./db/db.json', null, 2))
-  newNotes.push(body);
-  fs.writeFileSync('./db/db.json', JSON.stringify(newNotes, null, 2)), err => {
-    if (err) throw err;
-  };
-  res.send(newNotes);
-});
-
-// delete note
-app.delete('/api/notes/:id', (req, res) => {
+// DELETE note and update db.json
+app.delete("/api/notes/:id", (req, res) => {
   let id = req.params.id.toString();
-  let readNote = JSON.parse(fs.readFileSync("./db/db.json", null, 2));
-  let deleteNote = readNote.filter(note => note.id.toString() !== id);
-  fs.writeFileSync("./db/db.json", JSON.stringify(deleteNote, null, 2)), err => {
-    if (err) throw err;
-  };
-  res.sendStatus(200);
+  let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  const updatedNotes = data.filter( note => note.id.toString() !== id );
+  fs.writeFileSync("./db/db.json", JSON.stringify(updatedNotes));
+
+  res.json(updatedNotes);
 });
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
+
+
+
